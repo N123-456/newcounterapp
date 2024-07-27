65417f9b325a73c3fc45d1f96f92bd5763b391b9
Ayesha Nizam <ayeshanizam39@gmail.com>
2024-07-27 21:08:41 +0500

    counter

---
+++ src/Counter/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Counter.css';
+const Counter = () => {
+    const [count,setCount]=useState(0)
+  return (
+    <div className='counter'>
+    <div className="heading"><h1>{count}</h1></div>
+    <div className='btn-container'>
+    <button className='btn' onClick={()=>setCount(count+1)}>Increment</button>
+    <button className='btn' onClick={()=>setCount(count-1)} disabled={count==0}>Decrement</button>
+    <button className='btn' onClick={()=>setCount(0)}>Reset</button>
+    </div>
+  </div>
+  );
+}
+
+export default Counter;
