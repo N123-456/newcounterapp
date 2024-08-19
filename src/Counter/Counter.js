4248f7f65901529a74314a1a6253d780bd7aad28
Ayesha Nizam <ayeshanizam39@gmail.com>
2024-08-19 16:32:59 +0500

    Resolve Conflict

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
-import './Counter.css';
-import {
-  CitySelect,
-  CountrySelect,
-  StateSelect,
-  LanguageSelect,
-} from "react-country-state-city";
-import "react-country-state-city/dist/react-country-state-city.css";
-const data = {
-  USA: {
-    California: ['Los Angeles', 'San Francisco'],
-    Texas: ['Houston', 'Dallas'],
-  },
-  India: {
-    Maharashtra: ['Mumbai', 'Pune'],
-    Karnataka: ['Bangalore', 'Mysore'],
-  },
-};
+import React, { useState } from "react";
+import "./Counter.css";
+
 const Counter = () => {
- 
-
-
-
-   
-    
-    const [count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
   return (
-    <div className='counter'>
-    <div className="heading"><h1>{count}</h1></div>
-    <div className='btn-container'>
-    <button className='btn' onClick={()=>setCount(count+1)}>Increment</button>
-    <button className='btn' onClick={()=>setCount(count-1)} disabled={count==0}>Decrement</button>
-    <button className='btn' onClick={()=>setCount(0)}>Reset</button>
+    <div className="counter">
+      <div className="heading">
+        <h1>{count}</h1>
+      </div>
+      <div className="btn-container">
+        <button className="btn" onClick={() => setCount(count + 1)}>
+          Increment
+        </button>
+        <button
+          className="btn"
+          onClick={() => setCount(count - 1)}
+          disabled={count == 0}
+        >
+          Decrement
+        </button>
+        <button className="btn" onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Counter;
